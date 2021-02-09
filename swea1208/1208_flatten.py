d742b35e64afc3536ff8eff82dc0b1179ab3644f
soc4585 <soc4585@gmail.com>
2021-02-09 16:21:46 +0900

    swea1208번 문제풀이

---
+++ swea1208/1208_flatten.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open("input_1208_flatten.txt", "r")
+
+# T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+# for test_case in range(1, T + 1):
+
+for test_case in range(1, 11):
+    dump = int(input())
+    boxes = list(map(int, input().split()))
+
+    gap = 0
+    for _ in range(dump+1):
+        min_i = 0
+        max_i = 0
+        min_v = boxes[0]
+        max_v = boxes[0]
+
+        for i, v in enumerate(boxes):
+            if max_v < v:
+                max_v = v
+                max_i = i
+            if min_v > v:
+                min_v = v
+                min_i = i
+
+        gap = boxes[max_i] - boxes[min_i]
+        if gap <= 1:
+            break
+
+        boxes[max_i] -= 1
+        boxes[min_i] += 1
+
+    print('#{} {}'.format(test_case, gap))
