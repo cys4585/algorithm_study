8d400bcc19f1c127a75b7670ed75989afe73657c
soc4585 <soc4585@gmail.com>
2021-03-16 21:59:44 +0900

    swea4012 요리사 (역테 A형 대비)

--- problem/swea4012/swea_4012.py
+++ problem/swea4012/swea_4012.py
@@ -2,8 +2,33 @@
 # 난이도 ?
 import sys
 sys.stdin = open('input_4012.txt')
+
+def synergy(arr):
+    taste = 0
+    for i in arr:
+        for j in arr:
+            taste += table[i][j]
+    return taste
+
+def choice(idx, cnt):
+    global result
+    if cnt == N//2:
+        A, B = [], []
+        for i in range(N):
+            if used[i]: A.append(i)
+            else: B.append(i)
+        taste_A = synergy(A)
+        taste_B = synergy(B)
+        difference = abs(taste_A - taste_B)
+        if result is None or difference < result: result = difference
+        return
+    for i in range(idx, N):
+        if not used[i]:
+            used[i] = 1
+            choice(i, cnt + 1)
+            used[i] = 0
+
 for tc in range(1, int(input()) + 1):
-    N = int(input())    # N개의 식재료 (짝수)
     # N * N
     # 식재료들을 각각 N/2 개씩 나누어 두 개의 요리를 한다.
     # 각각의 음식 -> A, B
@@ -13,3 +38,11 @@ for tc in range(1, int(input()) + 1):
 
     # 두 음식 간의 맛의 차이(시너지의 합의 차)가 최소가 될 때의 최솟값 출력
 
+    N = int(input())    # N개의 식재료 (짝수)
+    table = [list(map(int, input().split())) for _ in range(N)]
+    # print(N, table)
+
+    used = [0] * N
+    result = None
+    choice(0, 0)
+    print('#{} {}'.format(tc, result))
