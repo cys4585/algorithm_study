9873cc23dff5483306deeb31583f0c6a65301aaa
soc4585 <soc4585@gmail.com>
2021-03-16 23:44:17 +0900

    swea10966 물놀이 (D4) 해결 / list()로 queue를 구현하다가, deque를 쓰니 시간초과문제를 해결할 수 있었음

--- problem/swea10966/swea_10966.py
+++ problem/swea10966/swea_10966.py
@@ -2,41 +2,33 @@
 # D4
 import sys
 sys.stdin = open('input_10966.txt')
-
-def find_water(sy, sx, scnt):
-    queue = list()
-    queue.append([sy, sx, scnt])
-    visited = [[0] * M for _ in range(N)]
-    while queue:
-        y, x, cnt = queue.pop(0)
-        visited[y][x] = 1
-        if in_map[y][x] == 'L':
-            if cnt_arr[y][x] == 0 or cnt_arr[y][x] > cnt:
-                cnt_arr[y][x] = cnt
-        for dir in direction:
-            ny, nx = y + dir[0], x + dir[1]
-            if 0 <= ny < N and 0 <= nx < M:
-                if visited[ny][nx]: continue
-                queue.append([ny, nx, cnt + 1])
+from collections import deque
 
 direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 for tc in range(1, int(input()) + 1):
     N, M = map(int, input().split())
-    in_map = [list(map(str, input())) for _ in range(N)]
-    # print(N, M, in_map)
-
-    # 땅으로 표현된 모든 칸(L)에서 어떤 물인 칸(W)으로 이동하기 위한 최소 이동 횟수 구하기
-    # 그 이동 횟수의 총합을 출력
-
-    cnt_arr = [[0] * M for _ in range(N)]
-
-    for i in range(N):
-        for j in range(M):
-            if in_map[i][j] == 'W': find_water(i, j, 0)
+    in_map = [input() for _ in range(N)]
 
     result = 0
+    water_locs = deque()
+    visited = [[0] * M for _ in range(N)]
     for i in range(N):
         for j in range(M):
-            result += cnt_arr[i][j]
+            if in_map[i][j] == 'W':
+                water_locs.append((i, j, 0))
+                visited[i][j] = 1
+
+    while water_locs:
+        # print(queue)
+        y, x, cnt = water_locs.popleft()
+        if in_map[y][x] == 'L': result += cnt
+        # print(y, x, cnt, result)
+        for dir in direction:
+            ny, nx = y + dir[0], x + dir[1]
+            if 0 <= ny < N and 0 <= nx < M:
+                if visited[ny][nx]: continue
+                visited[ny][nx] = 1
+                water_locs.append((ny, nx, cnt + 1))
+
     print('#{} {}'.format(tc, result))
