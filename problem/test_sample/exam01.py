6aafe8afbcbcddbbaf57b8eba429ef290e631728
soc4585 <soc4585@gmail.com>
2021-03-18 23:54:51 +0900

    모의역량테스트 1번문제... (testcase 49/50)

---
+++ problem/test_sample/exam01.py
@@ -0,0 +1,75 @@
+import sys
+sys.stdin = open('exam01.txt')
+
+direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상 하 좌 우
+
+def f(idx):
+    global result
+    if idx == len(need_connect):
+        # print('#'*50)
+        # print(check, len(need_connect))
+        # for a in in_arr:
+        #     print(a)
+        # print()
+        cnt = 0
+        for i in range(N):
+            for j in range(N):
+                if in_arr[i][j] == 2:
+                    cnt += 1
+        if result > cnt:
+            result = cnt
+        return
+    y, x = need_connect[idx]
+    check[idx] = 1
+    # f(idx + 1)
+    check[idx] = 0
+    # 4방향 탐색 /
+    for dir in direction:
+        ny, nx = y, x
+        possible = False
+        # 한 방향이 벽까지 갈 수 있는지 체크
+        while True:
+            ny, nx = ny + dir[0], nx + dir[1]
+            if in_arr[ny][nx] != 0:
+                break
+            if ny == 0 or ny == N-1 or nx == 0 or nx == N-1:
+                possible = True
+                break
+        ny, nx = y, x
+        # 끝까지 갈 수 있으면 전선 연결
+        if possible:
+            while True:
+                ny, nx = ny + dir[0], nx + dir[1]
+                if 0 <= ny < N and 0 <= nx < N:
+                    in_arr[ny][nx] = 2
+                else:
+                    break
+            check[idx] = 1
+            f(idx+1)
+            check[idx] = 0
+            while True:
+                ny, nx = ny - dir[0], nx - dir[1]
+                if y == ny and x == nx:
+                    break
+                else:
+                    in_arr[ny][nx] = 0
+
+
+
+for tc in range(1, int(input()) + 1):
+    N = int(input())
+    in_arr = [list(map(int, input().split())) for _ in range(N)]
+    # print(tc, N, in_arr)
+
+    need_connect = []
+    for r in range(N):
+        for c in range(N):
+            if r == 0 or r == N-1 or c == 0 or c == N-1: continue
+            if in_arr[r][c] == 1: need_connect.append([r, c])
+    check = [0] * len(need_connect)
+    if tc == 6: pass
+    result = 987654321
+    used = [False] * len(need_connect)
+    f(0)
+
+    print('#{} {}'.format(tc, result))
